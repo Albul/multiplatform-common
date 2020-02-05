0a307f78d9a581373916dff6a3646a146f6066fc
albul <abdula.ua@gmail.com>
2020-02-05 09:14:31 +0200

    Add bidi docs

--- multiplatform-common/src/commonMain/kotlin/com/olekdia/common/Constants.kt
+++ multiplatform-common/src/commonMain/kotlin/com/olekdia/common/Constants.kt
@@ -15,7 +15,8 @@ const val FIRST_GREATER = 1
 const val EQUALS = 0
 const val SECOND_GREATER = -1
 
-// Mime
+//--------------- Mime
+
 const val ANY_MIME = "*/*"
 const val TEXT_MIME = "text/plain"
 const val HTML_MIME = "text/html"
@@ -28,50 +29,72 @@ const val SCHEME_HTTP = "http"
 const val SCHEME_HTTPS = "https"
 const val UTF_8 = "UTF-8"
 
-// Math
+//--------------- Math
+/**
+ * 2 * PI in degrees
+ */
 const val DEG_2PI = 360F
 const val PI = 3.141592653589793F
 const val GOLDEN_RATIO = 1.61803F
 
-// Bidi
+//--------------- Bidi
 
 /**
+ * Unicode "Left-To-Right Isolate"
+ * Sets base direction to LTR and isolates the embedded content from the surrounding text
+ */
+const val LRI = '\u2066'
+/**
+ * Unicode "Right-To-Left Isolate"
+ * Sets base direction to RTL and isolates the embedded content from the surrounding text
+ */
+const val RLI = '\u2067'
+/**
+ * Unicode "First-Strong Isolate"
+ * Isolates the content and sets the direction according to the first strongly typed directional character
+ */
+const val FSI = '\u2068'
+/**
  * Unicode "Left-To-Right Embedding"
+ * Sets base direction to LTR but allows embedded text to interact with surrounding content,
+ * so risk of spillover effects
  */
 const val LRE = '\u202A'
 /**
  * Unicode "Right-To-Left Embedding"
+ * Sets base direction to RTL but allows embedded text to interact with surrounding content,
+ * so risk of spillover effects
  */
 const val RLE = '\u202B'
 /**
+ * Unicode "Left-To-Right Override"
+ * Overrides the bidirectional algorithm to display characters in memory order,
+ * progressing from left to right
+ */
+const val LRO = '\u202D'
+/**
+ * Unicode "Right-To-Left Override"
+ * Overrides the bidirectional algorithm to display characters in memory order,
+ * progressing from right to left
+ */
+const val RLO = '\u202E'
+/**
  * Unicode "Pop Directional Formatting"
+ * Used for RLE or LRE, RLO or LRO
  */
 const val PDF = '\u202C'
 /**
  * Unicode "Pop Directional Isolate"
+ * Used for RLI, LRI or FSI
  */
 const val PDI = '\u2069'
 /**
- * Left-To-Right Mark
+ * Unicode "Left-To-Right Mark"
+ * Strongly typed LTR character
  */
 const val LRM = '\u200E'
 /**
- * Right-To-Left Mark
+ * Unicode "Right-To-Left Mark"
+ * Strongly typed RTL character
  */
 const val RLM = '\u200F'
-/**
- * Left-To-Right Isolated
- */
-const val LRI = '\u2066'
-/**
- * Right-To-Left Isolated
- */
-const val RLI = '\u2067'
-/**
- * First-Strong Isolated
- */
-const val FSI = '\u2068'
-
-const val LRO = '\u202D'
-
-const val RLO = '\u202E'
