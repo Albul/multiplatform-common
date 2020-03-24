5f8ca8c491c71a30e6355c66460565fd1b8d546b
albul <abdula.ua@gmail.com>
2020-03-24 13:23:36 +0200

    Add CommonTest

--- multiplatform-common/src/commonMain/kotlin/com/olekdia/common/extensions/CommonExt.kt
+++ multiplatform-common/src/commonMain/kotlin/com/olekdia/common/extensions/CommonExt.kt
@@ -1,4 +1,5 @@
 @file:JvmName("CommonExt")
+
 package com.olekdia.common.extensions
 
 import kotlin.jvm.JvmName
@@ -7,22 +8,20 @@ inline fun <T, R> T?.ifNotNull(block: T.() -> R) {
     if (this != null) block()
 }
 
-inline fun <A, R> ifNotNull(a: A?, code: (A) -> R): R? {
-    return if (a != null) code(a) else null
-}
+inline fun <A, R> ifNotNull(a: A?, code: (A) -> R): R? =
+    if (a != null) code(a) else null
 
-inline fun <A, B, R> ifNotNull(a: A?, b: B?, code: (A, B) -> R): R? {
-    return if (a != null
+inline fun <A, B, R> ifNotNull(a: A?, b: B?, code: (A, B) -> R): R? =
+    if (a != null
         && b != null
     ) {
         code(a, b)
     } else {
         null
     }
-}
 
-inline fun <A, B, C, R> ifNotNull(a: A?, b: B?, c: C?, code: (A, B, C) -> R): R? {
-    return if (a != null
+inline fun <A, B, C, R> ifNotNull(a: A?, b: B?, c: C?, code: (A, B, C) -> R): R? =
+    if (a != null
         && b != null
         && c != null
     ) {
@@ -30,7 +29,17 @@ inline fun <A, B, C, R> ifNotNull(a: A?, b: B?, c: C?, code: (A, B, C) -> R): R?
     } else {
         null
     }
-}
+
+inline fun <A, B, C, D, R> ifNotNull(a: A?, b: B?, c: C?, d: D?, code: (A, B, C, D) -> R): R? =
+    if (a != null
+        && b != null
+        && c != null
+        && d != null
+    ) {
+        code(a, b, c, d)
+    } else {
+        null
+    }
 
 inline fun <T, R> T?.ifNull(block: T.() -> R) {
     if (this == null) block()
@@ -38,23 +47,22 @@ inline fun <T, R> T?.ifNull(block: T.() -> R) {
 
 inline fun <A, R> ifNotNullAnd(
     a: A?, exp1: Boolean, code: (A) -> R
-): R? {
-    return if (a != null
+): R? =
+    if (a != null
         && exp1
     ) {
         code(a)
     } else {
         null
     }
-}
 
 inline fun <A, B, R> ifNotNullAnd(
     a: A?,
     b: B?,
     exp1: Boolean,
     code: (A, B) -> R
-): R? {
-    return if (a != null
+): R? =
+    if (a != null
         && b != null
         && exp1
     ) {
@@ -62,7 +70,6 @@ inline fun <A, B, R> ifNotNullAnd(
     } else {
         null
     }
-}
 
 inline fun <A, B, C, R> ifNotNullAnd(
     a: A?,
@@ -70,8 +77,8 @@ inline fun <A, B, C, R> ifNotNullAnd(
     c: C?,
     exp1: Boolean,
     code: (A, B, C) -> R
-): R? {
-    return if (a != null
+): R? =
+    if (a != null
         && b != null
         && c != null
         && exp1
@@ -80,12 +87,11 @@ inline fun <A, B, C, R> ifNotNullAnd(
     } else {
         null
     }
-}
 
 inline fun <A, R> ifNotNullAnd(
     a: A?, exp1: Boolean, exp2: Boolean, code: (A) -> R
-): R? {
-    return if (a != null
+): R? =
+    if (a != null
         && exp1
         && exp2
     ) {
@@ -93,21 +99,25 @@ inline fun <A, R> ifNotNullAnd(
     } else {
         null
     }
-}
 
-inline fun <A, B, R> ifNotNullElse(a: A?, b: B?, ifCode: (A, B) -> R, elseCode: () -> R): R? {
-    return if (a != null
+inline fun <A, R> ifNotNullElse(a: A?, ifCode: (A) -> R, elseCode: () -> R): R? =
+    if (a != null) {
+        ifCode(a)
+    } else {
+        elseCode()
+    }
+
+inline fun <A, B, R> ifNotNullElse(a: A?, b: B?, ifCode: (A, B) -> R, elseCode: () -> R): R? =
+    if (a != null
         && b != null
     ) {
         ifCode(a, b)
     } else {
         elseCode()
     }
-}
 
-inline fun <T> T?.ifNotNullOrFalse(block: T.() -> Boolean): Boolean {
-    return if (this == null) false else block()
-}
+inline fun <T> T?.ifNotNullOrFalse(block: T.() -> Boolean): Boolean =
+    if (this == null) false else block()
 
 inline fun Boolean?.ifFalse(block: () -> Unit) {
     if (this == false) block()
---
+++ multiplatform-common/src/commonTest/kotlin/com/olekdia/common/extensions/CommonExtTest.kt
@@ -0,0 +1,582 @@
+package com.olekdia.common.extensions
+
+import kotlin.test.*
+
+class CommonExtTest {
+
+    @Test
+    fun ifNotNull_withNotNullReceiver() {
+        val someObj = ""
+
+        var isCalled = false
+        someObj.ifNotNull { isCalled = true }
+
+        assertTrue(isCalled)
+    }
+
+    @Test
+    fun ifNotNull_withNullReceiver() {
+        val someObj = null
+
+        var isCalled = false
+        someObj.ifNotNull { isCalled = true }
+
+        assertFalse(isCalled)
+    }
+
+    @Test
+    fun ifNull_withNotNullReceiver() {
+        val someObj = ""
+
+        var isCalled = false
+        someObj.ifNull { isCalled = true }
+
+        assertFalse(isCalled)
+    }
+
+    @Test
+    fun ifNull_withNullReceiver() {
+        val someObj = null
+
+        var isCalled = false
+        someObj.ifNull { isCalled = true }
+
+        assertTrue(isCalled)
+    }
+
+//--------------------------------------------------------------------------------------------------
+//  ifNotNull
+//--------------------------------------------------------------------------------------------------
+
+    @Test
+    fun ifNotNull_withNotNull1Arg() {
+        val arg1: String? = "1"
+
+        var isCalled = false
+        val result = ifNotNull(
+            arg1
+        ) { one ->
+            isCalled = true
+            "${one} called"
+        }
+
+        assertEquals("1 called", result)
+        assertTrue(isCalled)
+    }
+
+    @Test
+    fun ifNotNull_withNotNull2Args() {
+        val arg1: String? = "1"
+        val arg2: String? = "2"
+
+        var isCalled = false
+        val result = ifNotNull(
+            arg1, arg2
+        ) { one, two ->
+            isCalled = true
+            "${one} ${two} called"
+        }
+
+        assertEquals("1 2 called", result)
+        assertTrue(isCalled)
+    }
+
+    @Test
+    fun ifNotNull_withNotNull3Args() {
+        val arg1: String? = "1"
+        val arg2: String? = "2"
+        val arg3: String? = "3"
+
+        var isCalled = false
+        val result = ifNotNull(
+            arg1, arg2, arg3
+        ) { one, two, three ->
+            isCalled = true
+            "${one} ${two} ${three} called"
+        }
+
+        assertEquals("1 2 3 called", result)
+        assertTrue(isCalled)
+    }
+
+    @Test
+    fun ifNotNull_withNotNull4Args() {
+        val arg1: String? = "1"
+        val arg2: String? = "2"
+        val arg3: String? = "3"
+        val arg4: String? = "4"
+
+        var isCalled = false
+        val result = ifNotNull(
+            arg1, arg2, arg3, arg4
+        ) { one, two, three, four ->
+            isCalled = true
+            "${one} ${two} ${three} ${four} called"
+        }
+
+        assertEquals("1 2 3 4 called", result)
+        assertTrue(isCalled)
+    }
+
+    @Test
+    fun ifNotNull_withNull1Arg() {
+        val arg1: String? = null
+
+        var isCalled = false
+        val result = ifNotNull(
+            arg1
+        ) { one ->
+            isCalled = true
+            "${one} called"
+        }
+
+        assertNull(result)
+        assertFalse(isCalled)
+    }
+
+
+    @Test
+    fun ifNotNull_withNull2Args() {
+        val arg1: String? = "1"
+        val arg2: String? = null
+
+        var isCalled = false
+        val result = ifNotNull(
+            arg1, arg2
+        ) { one, two ->
+            isCalled = true
+            "${one} ${two} called"
+        }
+
+        assertNull(result)
+        assertFalse(isCalled)
+    }
+
+    @Test
+    fun ifNotNull_withNull3Args() {
+        val arg1: String? = "1"
+        val arg2: String? = "2"
+        val arg3: String? = null
+
+        var isCalled = false
+        val result = ifNotNull(
+            arg1, arg2, arg3
+        ) { one, two, three ->
+            isCalled = true
+            "${one} ${two} ${three} called"
+        }
+
+        assertNull(result)
+        assertFalse(isCalled)
+    }
+
+//--------------------------------------------------------------------------------------------------
+//  ifNotNullAnd
+//--------------------------------------------------------------------------------------------------
+
+    @Test
+    fun ifNotNullAnd_withNotNull1Arg_withTrueArg_called() {
+        val arg1: String? = "1"
+        val num = 3
+
+        var isCalled = false
+        val result = ifNotNullAnd(
+            arg1, num > 2
+        ) { one ->
+            isCalled = true
+            "${one} called"
+        }
+
+        assertEquals("1 called", result)
+        assertTrue(isCalled)
+    }
+
+    @Test
+    fun ifNotNullAnd_withNull1Arg_withTrueArg_notCalled() {
+        val arg1: String? = null
+        val num = 3
+
+        var isCalled = false
+        val result = ifNotNullAnd(
+            arg1, num > 2
+        ) { one ->
+            isCalled = true
+            "${one} called"
+        }
+
+        assertNull(result)
+        assertFalse(isCalled)
+    }
+
+    @Test
+    fun ifNotNullAnd_withNotNull1Arg_withFalseArg_notCalled() {
+        val arg1: String? = "1"
+        val num = 3
+
+        var isCalled = false
+        val result = ifNotNullAnd(
+            arg1, num < 2
+        ) { one ->
+            isCalled = true
+            "${one} called"
+        }
+
+        assertNull(result)
+        assertFalse(isCalled)
+    }
+
+    @Test
+    fun ifNotNullAnd_withNotNull2Args_withTrueArg_called() {
+        val arg1: String? = "1"
+        val arg2: String? = "2"
+        val num = 3
+
+        var isCalled = false
+        val result = ifNotNullAnd(
+            arg1, arg2, num > 2
+        ) { one, two ->
+            isCalled = true
+            "${one} ${two} called"
+        }
+
+        assertEquals("1 2 called", result)
+        assertTrue(isCalled)
+    }
+
+    @Test
+    fun ifNotNullAnd_withNull2Args_withTrueArg_notCalled() {
+        val arg1: String? = "1"
+        val arg2: String? = null
+        val num = 3
+
+        var isCalled = false
+        val result = ifNotNullAnd(
+            arg1, arg2, num > 2
+        ) { one, two ->
+            isCalled = true
+            "${one} ${two} called"
+        }
+
+        assertNull(result)
+        assertFalse(isCalled)
+    }
+
+    @Test
+    fun ifNotNullAnd_withNotNull2Args_withFalseArg_notCalled() {
+        val arg1: String? = "1"
+        val arg2: String? = "2"
+        val num = 3
+
+        var isCalled = false
+        val result = ifNotNullAnd(
+            arg1, arg2, num < 2
+        ) { one, two ->
+            isCalled = true
+            "${one} ${two} called"
+        }
+
+        assertNull(result)
+        assertFalse(isCalled)
+    }
+
+    @Test
+    fun ifNotNullAnd_withNotNull3Args_withTrueArg_called() {
+        val arg1: String? = "1"
+        val arg2: String? = "2"
+        val arg3: String? = "3"
+        val num = 3
+
+        var isCalled = false
+        val result = ifNotNullAnd(
+            arg1, arg2, arg3, num > 2
+        ) { one, two, three ->
+            isCalled = true
+            "${one} ${two} ${three} called"
+        }
+
+        assertEquals("1 2 3 called", result)
+        assertTrue(isCalled)
+    }
+
+    @Test
+    fun ifNotNullAnd_withNull3Args_withTrueArg_notCalled() {
+        val arg1: String? = "1"
+        val arg2: String? = null
+        val arg3: String? = "3"
+        val num = 3
+
+        var isCalled = false
+        val result = ifNotNullAnd(
+            arg1, arg2, arg3, num > 2
+        ) { one, two, three ->
+            isCalled = true
+            "${one} ${two} ${three} called"
+        }
+
+        assertNull(result)
+        assertFalse(isCalled)
+    }
+
+    @Test
+    fun ifNotNullAnd_withNotNull3Args_withFalseArg_notCalled() {
+        val arg1: String? = "1"
+        val arg2: String? = "2"
+        val arg3: String? = "3"
+        val num = 3
+
+        var isCalled = false
+        val result = ifNotNullAnd(
+            arg1, arg2, arg3, num < 2
+        ) { one, two, three ->
+            isCalled = true
+            "${one} ${two} ${three} called"
+        }
+
+        assertNull(result)
+        assertFalse(isCalled)
+    }
+
+    @Test
+    fun ifNotNullAnd_withNotNull1Arg_withTrue2Args_called() {
+        val arg1: String? = "1"
+        val bool1 = 3 > 2
+        val bool2 = 3 > 1
+
+        var isCalled = false
+        val result = ifNotNullAnd(
+            arg1, bool1, bool2
+        ) { one ->
+            isCalled = true
+            "${one} called"
+        }
+
+        assertEquals("1 called", result)
+        assertTrue(isCalled)
+    }
+
+    @Test
+    fun ifNotNullAnd_withNull1Arg_withTrue2Args_notCalled() {
+        val arg1: String? = null
+        val bool1 = 3 > 2
+        val bool2 = 3 > 1
+
+        var isCalled = false
+        val result = ifNotNullAnd(
+            arg1, bool1, bool2
+        ) { one ->
+            isCalled = true
+            "${one} called"
+        }
+
+        assertNull(result)
+        assertFalse(isCalled)
+    }
+
+    @Test
+    fun ifNotNullAnd_1arg_2boolExp_withNotNull1Arg_withFalse2Args_notCalled() {
+        val arg1: String? = "1"
+        val bool1 = 3 > 2
+        val bool2 = 3 > 5
+
+        var isCalled = false
+        val result = ifNotNullAnd(
+            arg1, bool1, bool2
+        ) { one ->
+            isCalled = true
+            "${one} called"
+        }
+
+        assertNull(result)
+        assertFalse(isCalled)
+    }
+
+//--------------------------------------------------------------------------------------------------
+//  ifNotNullElse
+//--------------------------------------------------------------------------------------------------
+
+    @Test
+    fun ifNotNullElse_withNotNull3Args() {
+        val arg: String? = "1"
+
+        var ifCalled = false
+        var elseCalled = false
+        val result = ifNotNullElse(
+            arg,
+            { one ->
+                ifCalled = true
+                "${one} if called"
+            },
+            {
+                elseCalled = true
+                "else called"
+            }
+        )
+
+        assertTrue(ifCalled)
+        assertFalse(elseCalled)
+        assertEquals("1 if called", result)
+    }
+
+    @Test
+    fun ifNotNullElse_withNull1Arg() {
+        val arg: String? = null
+
+        var ifCalled = false
+        var elseCalled = false
+        val result = ifNotNullElse(
+            arg,
+            { one ->
+                ifCalled = true
+                "${one} if called"
+            },
+            {
+                elseCalled = true
+                "else called"
+            }
+        )
+
+        assertFalse(ifCalled)
+        assertTrue(elseCalled)
+        assertEquals("else called", result)
+    }
+
+
+    @Test
+    fun ifNotNullElse_withNotNull2Args() {
+        val arg1: String? = "1"
+        val arg2: String? = "2"
+
+        var ifCalled = false
+        var elseCalled = false
+        val result = ifNotNullElse(
+            arg1, arg2,
+            { one, two ->
+                ifCalled = true
+                "${one} ${two} if called"
+            },
+            {
+                elseCalled = true
+                "else called"
+            }
+        )
+
+        assertTrue(ifCalled)
+        assertFalse(elseCalled)
+        assertEquals("1 2 if called", result)
+    }
+
+    @Test
+    fun ifNotNullElse_withNull2Args() {
+        val arg1: String? = "1"
+        val arg2: String? = null
+
+        var ifCalled = false
+        var elseCalled = false
+        val result = ifNotNullElse(
+            arg1, arg2,
+            { one, two ->
+                ifCalled = true
+                "${one} ${two} if called"
+            },
+            {
+                elseCalled = true
+                "else called"
+            }
+        )
+
+        assertFalse(ifCalled)
+        assertTrue(elseCalled)
+        assertEquals("else called", result)
+    }
+
+//--------------------------------------------------------------------------------------------------
+
+    @Test
+    fun ifNotNullOrFalse_withNotNullReceiver() {
+        val receiver: String? = "1"
+
+        var isCalled = false
+        val result = receiver.ifNotNullOrFalse {
+            isCalled = true
+            isCalled
+        }
+
+        assertTrue(isCalled)
+        assertTrue(result)
+    }
+    @Test
+    fun ifNotNullOrFalse_withNullReceiver() {
+        val receiver: String? = null
+
+        var isCalled = false
+        val result = receiver.ifNotNullOrFalse {
+            isCalled = true
+            true
+        }
+
+        assertFalse(isCalled)
+        assertFalse(result)
+    }
+
+    @Test
+    fun ifFalse_withFalseReceiver() {
+        val value: Boolean? = false
+
+        var isCalled = false
+        value.ifFalse { isCalled = true }
+
+        assertTrue(isCalled)
+    }
+
+    @Test
+    fun ifFalse_withTrueReceiver() {
+        val value: Boolean? = true
+
+        var isCalled = false
+        value.ifFalse { isCalled = true }
+
+        assertFalse(isCalled)
+    }
+
+    @Test
+    fun ifFalse_withNullReceiver() {
+        val value: Boolean? = null
+
+        var isCalled = false
+        value.ifFalse { isCalled = true }
+
+        assertFalse(isCalled)
+    }
+
+    @Test
+    fun ifTrue_withFalseReceiver() {
+        val value: Boolean? = false
+
+        var isCalled = false
+        value.ifTrue { isCalled = true }
+
+        assertFalse(isCalled)
+    }
+
+    @Test
+    fun ifTrue_withTrueReceiver() {
+        val value: Boolean? = true
+
+        var isCalled = false
+        value.ifTrue { isCalled = true }
+
+        assertTrue(isCalled)
+    }
+
+    @Test
+    fun ifTrue_withNullReceiver() {
+        val value: Boolean? = null
+
+        var isCalled = false
+        value.ifTrue { isCalled = true }
+
+        assertFalse(isCalled)
+    }
+
+
+}
