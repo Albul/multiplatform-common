d27a068214d0f4a667514e18b445f9df1166994e
albul <oleks.albul@gmail.com>
2020-06-05 19:13:05 +0300

    Add ColorExt

---
+++ multiplatform-common/src/commonMain/kotlin/com/olekdia/common/extensions/ColorExt.kt
@@ -0,0 +1,315 @@
+@file:JvmName("ColorExt")
+package com.olekdia.common.extensions
+
+import kotlin.jvm.JvmName
+import kotlin.jvm.JvmOverloads
+import kotlin.math.floor
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.math.roundToInt
+import kotlin.random.Random
+
+const val COLOR_ALPHA_MASK = 0x00FFFFFF
+const val MAX_COLOR_COMPONENT: Int = 0xFF
+const val MAX_COLOR_COMPONENT_F: Float = 255F
+
+inline fun Float.toIntColorComponent(): Int =
+    (this * MAX_COLOR_COMPONENT_F + 0.5F).toInt()
+
+inline fun Int.toFloatColorComponent(): Float =
+    this / MAX_COLOR_COMPONENT_F
+
+//--------------------------------------------------------------------------------------------------
+//  Getters of components
+//--------------------------------------------------------------------------------------------------
+
+/**
+ * Return the alpha component of a color int
+ */
+inline val Int.alpha: Int
+    get() = (this shr 24) and MAX_COLOR_COMPONENT
+
+/**
+ * Return the red component of a color int
+ */
+inline val Int.red: Int
+    get() = (this shr 16) and MAX_COLOR_COMPONENT
+
+/**
+ * Return the green component of a color int
+ */
+inline val Int.green: Int
+    get() = (this shr 8) and MAX_COLOR_COMPONENT
+
+/**
+ * Return the blue component of a color int
+ */
+inline val Int.blue: Int
+    get() = this and MAX_COLOR_COMPONENT
+
+/**
+ * Return the hue component of a ARGB color int
+ */
+val Int.hue: Float
+    get() = let(
+        this.red.toFloatColorComponent(), this.green.toFloatColorComponent(), this.blue.toFloatColorComponent()
+    ) { r, g, b ->
+        //	Minimum and Maximum RGB values are used in the HSL calculations
+        val min: Float = minOf(r, g, b)
+        val max: Float = maxOf(r, g, b)
+
+        when (max) {
+            min -> 0F
+            r -> (60 * (g - b) / (max - min) + 360) % 360
+            g -> 60 * (b - r) / (max - min) + 120
+            b -> 60 * (r - g) / (max - min) + 240
+            else -> 0F
+        }
+    }
+
+/**
+ * Return the saturation component of a ARGB color int
+ */
+val Int.saturation: Float
+    get() = let(
+        this.red, this.green, this.blue
+    ) { r, g, b ->
+        val min: Int = minOf(r, g, b)
+        val max: Int = maxOf(r, g, b)
+
+        if (max != 0) {
+            (max - min).toFloat() / max.toFloat()
+        } else {
+            0F
+        }
+    }
+
+/**
+ * Return the brightness component of a ARGB color int
+ */
+val Int.value: Float
+    get() = this.brightness
+
+/**
+ * Return the brightness component of a ARGB color int
+ */
+val Int.lightness: Float
+    get() = this.brightness
+
+/**
+ * Return the brightness component of a ARGB color int
+ */
+val Int.brightness: Float
+    get() = let(
+        this.red, this.green, this.blue
+    ) { r, g, b ->
+        maxOf(r, g, b) / MAX_COLOR_COMPONENT_F
+    }
+
+//--------------------------------------------------------------------------------------------------
+//  Color constructors
+//--------------------------------------------------------------------------------------------------
+
+/**
+ * Return a color-int from red, green, blue components.
+ * The alpha component is implicitly 255 (fully opaque).
+ * These component values should be \([0..255]\), but there is no
+ * range check performed, so if they are out of range, the
+ * returned color is undefined.
+ *
+ * @param red  Red component \([0..255]\) of the color
+ * @param green Green component \([0..255]\) of the color
+ * @param blue  Blue component \([0..255]\) of the color
+ */
+fun rgb(red: Int, green: Int, blue: Int): Int =
+    0xFF000000.toInt() or (red shl 16) or (green shl 8) or blue
+
+/**
+ * Return a color-int from red, green, blue float components
+ * in the range \([0..1]\). The alpha component is implicitly
+ * 1.0 (fully opaque). If the components are out of range, the
+ * returned color is undefined.
+ *
+ * @param red Red component \([0..1]\) of the color
+ * @param green Green component \([0..1]\) of the color
+ * @param blue Blue component \([0..1]\) of the color
+ */
+fun rgb(red: Float, green: Float, blue: Float): Int =
+    rgb(red.toIntColorComponent(), green.toIntColorComponent(), blue.toIntColorComponent())
+
+/**
+ * Return a color-int from alpha, red, green, blue components.
+ * These component values should be \([0..255]\), but there is no
+ * range check performed, so if they are out of range, the
+ * returned color is undefined.
+ * @param alpha Alpha component \([0..255]\) of the color
+ * @param red Red component \([0..255]\) of the color
+ * @param green Green component \([0..255]\) of the color
+ * @param blue Blue component \([0..255]\) of the color
+ */
+fun argb(alpha: Int, red: Int, green: Int, blue: Int): Int =
+    alpha shl 24 or (red shl 16) or (green shl 8) or blue
+
+/**
+ * Return a color-int from alpha, red, green, blue float components
+ * in the range \([0..1]\). If the components are out of range, the
+ * returned color is undefined.
+ *
+ * @param alpha Alpha component \([0..1]\) of the color
+ * @param red Red component \([0..1]\) of the color
+ * @param green Green component \([0..1]\) of the color
+ * @param blue Blue component \([0..1]\) of the color
+ */
+fun argb(alpha: Float, red: Float, green: Float, blue: Float): Int =
+    argb(
+        alpha.toIntColorComponent(),
+        red.toIntColorComponent(),
+        green.toIntColorComponent(),
+        blue.toIntColorComponent()
+    )
+
+/**
+ * Return a color-int from hue, saturation, brightness components of HSB model
+ * @param hue the hue component of the color
+ * @param saturation the saturation of the color
+ * @param brightness the brightness of the color
+ */
+fun hsb(hue: Float, saturation: Float, brightness: Float): Int {
+    var r = 0
+    var g = 0
+    var b = 0
+
+    if (saturation == 0F) {
+        b = brightness.toIntColorComponent()
+        g = b
+        r = g
+    } else {
+        val h = (hue - floor(hue.toDouble()).toFloat()) * 6.0F
+        val f = h - floor(g.toDouble()).toFloat()
+        val p = brightness * (1.0F - saturation)
+        val q = brightness * (1.0F - saturation * f)
+        val t = brightness * (1.0F - saturation * (1.0F - f))
+        when (h.toInt()) {
+            0 -> {
+                r = brightness.toIntColorComponent()
+                g = t.toIntColorComponent()
+                b = p.toIntColorComponent()
+            }
+            1 -> {
+                r = q.toIntColorComponent()
+                g = brightness.toIntColorComponent()
+                b = p.toIntColorComponent()
+            }
+            2 -> {
+                r = p.toIntColorComponent()
+                g = brightness.toIntColorComponent()
+                b = t.toIntColorComponent()
+            }
+            3 -> {
+                r = p.toIntColorComponent()
+                g = q.toIntColorComponent()
+                b = brightness.toIntColorComponent()
+            }
+            4 -> {
+                r = t.toIntColorComponent()
+                g = p.toIntColorComponent()
+                b = brightness.toIntColorComponent()
+            }
+            5 -> {
+                r = brightness.toIntColorComponent()
+                g = p.toIntColorComponent()
+                b = q.toIntColorComponent()
+            }
+        }
+    }
+
+    return rgb(r, g, b)
+}
+
+@JvmOverloads
+fun getRandomColor(mix: Int = 0x00FFFFFF): Int = rgb(
+    (Random.nextDouble() * MAX_COLOR_COMPONENT + mix.red).toInt() / 2,
+    (Random.nextDouble() * MAX_COLOR_COMPONENT + mix.green).toInt() / 2,
+    (Random.nextDouble() * MAX_COLOR_COMPONENT + mix.blue).toInt() / 2
+)
+
+//--------------------------------------------------------------------------------------------------
+//  Determine characteristics
+//--------------------------------------------------------------------------------------------------
+
+fun Int.isDarkColor(): Boolean =
+    (1 - (0.299 * this.red + 0.587 * this.green + 0.114 * this.blue) / MAX_COLOR_COMPONENT)
+        .let { darkness: Double ->
+            darkness >= 0.5
+        }
+
+//--------------------------------------------------------------------------------------------------
+//  Adjust color methods
+//--------------------------------------------------------------------------------------------------
+
+/**
+ * Return the ARGB color with adjusted alpha
+ */
+fun Int.adjustAlpha(factor: Float): Int =
+    (this.ushr(24) * factor).roundToInt() shl 24 or (COLOR_ALPHA_MASK and this)
+
+/**
+ * Return the ARGB color with new alpha [0...255]
+ */
+fun Int.withAlpha(alpha: Int): Int =
+    alpha shl 24 or (COLOR_ALPHA_MASK and this)
+
+/**
+ * Return the ARGB color with new alpha [0...1]
+ */
+fun Int.withAlpha(alpha: Float): Int =
+    this.withAlpha(alpha.toIntColorComponent())
+
+/**
+ * Return the ARGB color with new lightness [0...1] (of HSB model)
+ */
+fun Int.withLightness(lightness: Float): Int =
+    hsb(this.hue, this.saturation, lightness)
+
+/**
+ * Return the ARGB color with adjusted darkness
+ */
+fun Int.adjustDarkness(factor: Float): Int =
+    argb(
+        this.alpha,
+        min(max((this.red * factor).toInt(), 0), MAX_COLOR_COMPONENT),
+        min(max((this.green * factor).toInt(), 0), MAX_COLOR_COMPONENT),
+        min(max((this.blue * factor).toInt(), 0), MAX_COLOR_COMPONENT)
+    )
+
+/**
+ * Return the ARGB color with adjusted saturation [0...1] (of HSB model)
+ */
+fun Int.adjustSaturation(factor: Float): Int =
+    hsb(this.hue, this.saturation * factor, this.brightness)
+
+/**
+ * Return not transparent version of color
+ */
+fun Int.toOpaqueColor(): Int =
+    MAX_COLOR_COMPONENT shl 24 or (COLOR_ALPHA_MASK and this)
+
+/**
+ * Return a pastel version of color
+ */
+fun Int.toPastelColor(): Int =
+    rgb(
+        (this.red + MAX_COLOR_COMPONENT) / 2,
+        (this.green + MAX_COLOR_COMPONENT) / 2,
+        (this.blue + MAX_COLOR_COMPONENT) / 2
+    )
+
+/**
+ * Return a mixed version of color with other color
+ */
+fun Int.toMixedColor(componentToMix: Int): Int =
+    rgb(
+        (this.red + componentToMix) / 2,
+        (this.green + componentToMix) / 2,
+        (this.blue + componentToMix) / 2
+    )
---
+++ multiplatform-common/src/commonTest/kotlin/com/olekdia/common/extensions/ColorExtTest.kt
@@ -0,0 +1,161 @@
+package com.olekdia.common.extensions
+
+import kotlin.math.abs
+import kotlin.test.*
+
+class ColorExtTest {
+
+    fun assertEquals(expected: Float, actual: Float, delta: Float) {
+        assertTrue(abs(actual - expected) <= delta)
+    }
+
+    @Test
+    fun alphaTest() {
+        assertEquals(0xAB, 0xAB342321.toInt().alpha)
+        assertEquals(0x12, 0x12342321.alpha)
+        assertEquals(0x02, 0x02342321.alpha)
+        assertEquals(0x11, 0x11342321.alpha)
+        assertEquals(0, 0x00342321.alpha)
+    }
+
+    @Test
+    fun redTest() {
+        assertEquals(0x34, 0x11342321.red)
+        assertEquals(0x11, 0x12112321.red)
+        assertEquals(0x01, 0x02012321.red)
+        assertEquals(0, 0x11002321.red)
+        assertEquals(0xFF, 0x00FF2321.red)
+    }
+
+    @Test
+    fun greenTest() {
+        assertEquals(0x23, 0x11342321.green)
+        assertEquals(0x11, 0x12231121.green)
+        assertEquals(0x01, 0x02340121.green)
+        assertEquals(0, 0x11550021.green)
+        assertEquals(0xFF, 0x0021FF21.green)
+        assertEquals(0xFF, 0x0000FF00.green)
+    }
+
+    @Test
+    fun blueTest() {
+        assertEquals(0x23, 0x11348823.blue)
+        assertEquals(0x11, 0x12238811.blue)
+        assertEquals(0x01, 0x02348801.blue)
+        assertEquals(0, 0x11558800.blue)
+        assertEquals(0xFF, 0x002188FF.blue)
+        assertEquals(0xFF, 0x000000FF.blue)
+    }
+
+    @Test
+    fun hueTest() {
+        val delta = 0.0001F
+        assertEquals(6.315796F, 0x11342321.hue, delta)
+        assertEquals(127.05882F, 0x00119921.hue, delta)
+        assertEquals(352.37286F, 0x00881221.hue, delta)
+        assertEquals(246.76692F, 0x00211297.hue, delta)
+
+        assertEquals(0.0F, 0x00650000.hue, delta)
+        assertEquals(120.0F, 0x00008500.hue, delta)
+        assertEquals(240.0F, 0x00000085.hue, delta)
+        assertEquals(300.0F, 0x11850085.hue, delta)
+        assertEquals(0.0F, 0.hue, delta)
+        assertEquals(0.0F, 0xFFFFFFFF.toInt().hue, delta)
+        assertEquals(60.0F, 0xFFFFFF25.toInt().hue, delta)
+    }
+
+    @Test
+    fun saturationTest() {
+        val delta = 0.0001F
+        assertEquals(0.3653846F, 0x11342321.saturation, delta)
+        assertEquals(0.8888889F, 0x00119921.saturation, delta)
+        assertEquals(0.86764705F, 0x00881221.saturation, delta)
+        assertEquals(0.8807947F, 0x00211297.saturation, delta)
+
+        assertEquals(1.0F, 0x00650000.saturation, delta)
+        assertEquals(1.0F, 0x00008500.saturation, delta)
+        assertEquals(1.0F, 0x00000085.saturation, delta)
+        assertEquals(1.0F, 0x11850085.saturation, delta)
+        assertEquals(0.0F, 0.saturation, delta)
+        assertEquals(0.0F, 0xFFFFFFFF.toInt().saturation, delta)
+        assertEquals(0.85490197F, 0xFFFFFF25.toInt().saturation, delta)
+    }
+
+    @Test
+    fun brightnessTest() {
+        val delta = 0.0001F
+        assertEquals(0.20392157F, 0x11342321.brightness, delta)
+        assertEquals(0.6F, 0x00119921.brightness, delta)
+        assertEquals(0.53333336F, 0x00881221.brightness, delta)
+        assertEquals(0.5921569F, 0x00211297.brightness, delta)
+
+        assertEquals(0.39607844F, 0x00650000.brightness, delta)
+        assertEquals(0.52156866F, 0x00008500.brightness, delta)
+        assertEquals(0.52156866F, 0x00000085.brightness, delta)
+        assertEquals(0.52156866F, 0x11850085.brightness, delta)
+        assertEquals(0.0F, 0.brightness, delta)
+        assertEquals(1.0F, 0xFFFFFFFF.toInt().brightness, delta)
+        assertEquals(1.0F, 0xFFFFFF25.toInt().brightness, delta)
+    }
+
+    @Test
+    fun rgbOfIntsTest() {
+        assertEquals(0xFF342321.toInt(), rgb(0x34, 0x23, 0x21))
+        assertEquals(0xFF550001.toInt(), rgb(0x55, 0x00, 0x01))
+        assertEquals(0xFF55FF01.toInt(), rgb(0x55, 0xFF, 0x01))
+        assertEquals(0xFF0000FF.toInt(), rgb(0x00, 0x00, 0xFF))
+        assertEquals(0xFFFF0000.toInt(), rgb(0xFF, 0x00, 0x00))
+        assertEquals(0xFF000000.toInt(), rgb(0, 0, 0))
+    }
+
+    @Test
+    fun getRandomColorTest() {
+        val set = mutableSetOf<Int>()
+
+        for (i in 1..100) {
+            set.add(getRandomColor())
+        }
+        assertEquals(100, set.size) // All unique
+    }
+
+    @Test
+    fun toOpaqueColorTest() {
+        assertEquals(0xFF342321.toInt(), 0x12342321.toOpaqueColor())
+        assertEquals(0xFF00FF00.toInt(), 0x2300FF00.toOpaqueColor())
+        assertEquals(0xFF0000FF.toInt(), 0x310000FF.toOpaqueColor())
+        assertEquals(0xFFFF0000.toInt(), 0x86FF0000.toInt().toOpaqueColor())
+    }
+
+    @Test
+    fun isDarkColor() {
+        assertTrue(0xFF990000.toInt().isDarkColor())
+        assertTrue(0xFF808000.toInt().isDarkColor())
+        assertTrue(0xFF556B2F.toInt().isDarkColor())
+        assertTrue(0xFF006400.toInt().isDarkColor())
+        assertTrue(0xFF4682B4.toInt().isDarkColor())
+        assertTrue(0xFF191970.toInt().isDarkColor())
+        assertTrue(0xFF483D8B.toInt().isDarkColor())
+        assertTrue(0xFF8B4513.toInt().isDarkColor())
+        assertTrue(0xFF228B22.toInt().isDarkColor())
+        assertTrue(0xFF696969.toInt().isDarkColor())
+        assertTrue(0xFF708090.toInt().isDarkColor())
+
+
+        assertFalse(0xFFFFFF00.toInt().isDarkColor())
+        assertFalse(0xFFF0E68C.toInt().isDarkColor())
+        assertFalse(0xFFADFF2F.toInt().isDarkColor())
+        assertFalse(0xFF98FB98.toInt().isDarkColor())
+        assertFalse(0xFF20B2AA.toInt().isDarkColor())
+        assertFalse(0xFF00CED1.toInt().isDarkColor())
+        assertFalse(0xFFF5DEB3.toInt().isDarkColor())
+        assertFalse(0xFFB0C4DE.toInt().isDarkColor())
+        assertFalse(0xFFC0C0C0.toInt().isDarkColor())
+        assertFalse(0xFFF0FFFF.toInt().isDarkColor())
+    }
+
+    @Test
+    fun withAlphaTest() {
+        assertEquals(0xAB00FF00.toInt(), 0xFF00FF00.toInt().withAlpha(0xAB))
+        assertEquals(0xCD12FF25.toInt(), 0x2412FF25.toInt().withAlpha(0xCD))
+    }
+}
