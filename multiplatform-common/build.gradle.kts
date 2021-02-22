0dd9aa4d41a3f4982af433a3e90126e5b1708dfd
Oleksandr <oleks.albul@gmail.com>
2021-02-22 19:14:15 +0200

    commit

--- multiplatform-common/build.gradle.kts
+++ multiplatform-common/build.gradle.kts
@@ -1,5 +1,11 @@
+import java.io.FileInputStream
+import java.util.Properties
+import org.gradle.api.publish.PublishingExtension
+
 plugins {
     id("kotlin-multiplatform")
+    `maven-publish`
+    signing
 }
 
 kotlin {
@@ -7,7 +13,6 @@ kotlin {
         jvm()
         js() {
             browser()
-            nodejs()
         }
         linuxX64()
         linuxArm64()
@@ -73,6 +78,122 @@ kotlin {
             dependsOn(nativeMain)
         }
     }
+
+//    targets {
+//        jvm() {
+//            /* ... */
+//            mavenPublication {
+//                //artifactId = 'sample-lib-jvm'
+//                artifact(jvmJavadocJar)
+//            }
+//        }
+//    }
 }
 
-apply(from = "maven.publish.gradle.kts")
+tasks {
+    val javadocsJar by creating(Jar::class) {
+        archiveClassifier.set("javadoc")
+        from(JavaPlugin.JAVADOC_TASK_NAME)
+    }
+}
+
+//--------------------------------------------------------------------------------------------------
+//  Publishing
+//--------------------------------------------------------------------------------------------------
+
+val fis = FileInputStream("local.properties")
+val properties = Properties().apply {
+    load(fis)
+}
+val ossUser = properties.getProperty("oss.user")
+val ossPassword = properties.getProperty("oss.password")
+extra["signing.keyId"] = properties.getProperty("signing.keyId")
+extra["signing.password"] = properties.getProperty("signing.password")
+extra["signing.secretKeyRingFile"] = properties.getProperty("signing.secretKeyRingFile")
+
+val libraryVersion: String by project
+val publishedGroupId: String by project
+val artifactName: String by project
+val libraryName: String by project
+val libraryDescription: String by project
+val siteUrl: String by project
+val gitUrl: String by project
+val licenseName: String by project
+val licenseUrl: String by project
+val developerOrg: String by project
+val developerName: String by project
+val developerEmail: String by project
+val developerId: String by project
+
+project.group = publishedGroupId
+project.version = libraryVersion
+
+afterEvaluate {
+    configure<PublishingExtension> {
+        publications.all {
+            val mavenPublication = this as? MavenPublication
+            mavenPublication?.artifactId =
+                "${project.name}${"-$name".takeUnless { "kotlinMultiplatform" in name }.orEmpty()}"
+        }
+    }
+}
+
+configure<PublishingExtension> {
+    publications {
+        withType<MavenPublication> {
+            groupId = publishedGroupId
+            artifactId = artifactName
+            version = libraryVersion
+
+            artifact(tasks.findByName("javadocsJar"))
+            artifact(tasks.findByName("sourcesJar"))
+
+            pom {
+                name.set(libraryName)
+                description.set(libraryDescription)
+                url.set(siteUrl)
+
+                licenses {
+                    license {
+                        name.set(licenseName)
+                        url.set(licenseUrl)
+                    }
+                }
+                developers {
+                    developer {
+                        id.set(developerId)
+                        name.set(developerName)
+                        email.set(developerEmail)
+                    }
+                }
+                organization {
+                    name.set(developerOrg)
+                }
+                scm {
+                    connection.set(gitUrl)
+                    developerConnection.set(gitUrl)
+                    url.set(siteUrl)
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven("https://oss.sonatype.org/service/local/staging/deploy/maven2/") {
+            name = "sonatype"
+            credentials {
+                username = ossUser
+                password = ossPassword
+            }
+        }
+    }
+}
+
+signing {
+    //sign(publishing.publications)
+
+    publishing.publications.forEach { publication ->
+        sign(publication)
+    }
+   // sign(configurations.archives.get().filterNot { it.name.contains("dokka") })
+}
